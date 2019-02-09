31f1be0e918ffdc9db201b8679fdd22c4e794963
BOG-A209-E-017 <BOG-A209-E-017@RGH-LS6-17.UMD.LOCAL.EDU>
2019-02-09 08:11:52 -0500

    Primer Taller

---
+++ mavenproject1/src/main/java/HelloWorld.java
@@ -0,0 +1,27 @@
+
+import org.joda.time.LocalTime;
+
+
+
+
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author BOG-A209-E-017
+ */
+public class HelloWorld {
+    
+    public static  void  main(String[] args){
+        
+        LocalTime tiempo = new LocalTime();
+        System.out.println("el tiempo es"+tiempo);
+    }
+    
+}
